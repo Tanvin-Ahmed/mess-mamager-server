d4f36a48464196fb3605bb5b3f22e663365acdfd
Tanvin Ahmed <tanvinahmedtouhid91@gmail.com>
2022-12-04 15:26:21 +0600

    delete bin file

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-#!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
-
-const app = require("../app").app;
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const http = require("http");
 const debug = require("debug")("socketiotest:server");
+require("./src/db/db");
+
+const indexRouter = require("./src/routes/index");
+const usersRouter = require("./src/routes/users");
+const messRouter = require("./src/routes/mess");
+const messageRouter = require("./src/routes/chats");
+const { socketServerConnetion } = require("./src/socket/socket");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/mess", messRouter);
+app.use("/message", messageRouter);
+
+const server = http.createServer(app);
+socketServerConnetion(server);
+
+//**********  www file code *****************//
 
 /**
  * Get port from environment and store in Express.
  */
-
 const port = normalizePort(process.env.PORT || "5000");
 app.set("port", port);
 
 /**
  * Create HTTP server.
  */
-
-const server = require("../app").server;
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -64,11 +83,9 @@ function onError(error) {
     case "EACCES":
       console.error(bind + " requires elevated privileges");
       process.exit(1);
-      break;
     case "EADDRINUSE":
       console.error(bind + " is already in use");
       process.exit(1);
-      break;
     default:
       throw error;
   }
